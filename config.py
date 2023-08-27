e8552f9caaaef846badf436230dc173d973b5a76
Mr Davids <hellofucking@icloud.com>
2023-08-27 13:39:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -14,12 +14,12 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/50a2d5cffafa828ee9f76.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/fancygetchat")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/cerenasistanchannel")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 

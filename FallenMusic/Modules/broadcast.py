82a609ac84ccf644b6567da81448f4061a1fcf05
Mr Davids <hellofucking@icloud.com>
2023-10-28 13:06:38 +0300

    Update broadcast.py

--- FallenMusic/Modules/broadcast.py
+++ FallenMusic/Modules/broadcast.py
@@ -1,65 +1,48 @@
-# MIT License
-#
-# Copyright (c) 2023 AnonymousX1025
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import asyncio
-
+from config import OWNER_ID
+from FallenMusic import app
 from pyrogram import filters
-from pyrogram.errors import FloodWait
+from FallenMusic.Helpers.database.chatsdb import get_served_chats
+
+from pyrogram.errors import FloodWait 
 from pyrogram.types import Message
 
-from config import OWNER_ID
-from FallenMusic import app, app2
 
 
-@app.on_message(filters.command("broadcast") & filters.user(OWNER_ID))
-async def broadcast(_, message: Message):
-    brep = await message.reply_text("sᴛᴀʀᴛᴇᴅ ᴀssɪsᴛᴀɴᴛ ʙʀᴏᴀᴅᴄᴀsᴛ...")
+@app.on_message(filters.command(["broadcast"]) & filters.user(OWNER_ID))
+async def broadcast(_, message):
     if message.reply_to_message:
         x = message.reply_to_message.id
         y = message.chat.id
     else:
         if len(message.command) < 2:
             return await message.reply_text(
-                "**ᴇxᴀᴍᴘʟᴇ:**\n\n/broadcast [ᴍᴇssᴀɢᴇ] ᴏʀ [ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ]"
+                "**Kullanım**:\n/broadcast [MESSAGE] And [Mesage Respond]"
             )
         query = message.text.split(None, 1)[1]
     sent = 0
     chats = []
-    async for dialog in app2.get_dialogs():
-        chats.append(int(dialog.chat.id))
+    schats = await get_served_chats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
     for i in chats:
         try:
-            await app2.forward_messages(
+            await app.forward_messages(
                 i, y, x
-            ) if message.reply_to_message else await app2.send_message(i, text=query)
+            ) if message.reply_to_message else await app.send_message(
+                i, text=query
+            )
             sent += 1
         except FloodWait as e:
-            flood_time = int(e.value)
+            flood_time = int(e.x)
             if flood_time > 200:
                 continue
             await asyncio.sleep(flood_time)
         except Exception:
             continue
     try:
-        await brep.edit_text(f"**ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴍᴇssᴀɢᴇ ɪɴ {sent} ᴄʜᴀᴛs.**")
+        await message.reply_text(
+            f"**{sent} SuccesFully Send ✅.**"
+        )
     except:
-        await message.reply_text(f"**ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴍᴇssᴀɢᴇ ɪɴ {sent} ᴄʜᴀᴛs.**")
+        pass
